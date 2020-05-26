05043aed294c15b692359d5685c378646dc0be6c
Will <wsolutiontech@gmail.com>
2020-05-26 16:07:18 -0300

    Customizando dados de exibição, formato de datas, valores, localidade, entre outros

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 
@@ -9,13 +10,25 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        // Customizando Email
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        // Customizando Nomes na tela
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        // Customizando Nomes na tela
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Departament { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
+         
         public Seller ()
         {
 
